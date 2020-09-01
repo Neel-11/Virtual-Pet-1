8472a7a1f0a4978841dc4d30a22f4b7e9cdaaaa2
Neel-11 <67459867+Neel-11@users.noreply.github.com>
2020-09-01 08:28:21 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+//Create variables here
+var dog,happyDog,database,foodS,foodStock;
+
+function preload()
+{
+	//load images here
+}
+
+function setup() {
+	createCanvas(800, 700);
+  
+}
+
+
+function draw() {  
+
+  drawSprites();
+  //add styles here
+
+}
+
+
+
